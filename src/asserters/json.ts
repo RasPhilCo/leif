a21bd441510d06d8d2c787e4c1d6c449eca4f8fc
Mike Donnalley <mdonnalley@salesforce.com>
2021-02-04 09:47:58 -0700

    feat: add target_glob_filepath to json asserter (#35)

--- src/asserters/json.ts
+++ src/asserters/json.ts
@@ -1,5 +1,6 @@
 import * as path from 'path'
 import * as fs from 'fs-extra'
+import * as glob from 'fast-glob'
 
 import AsserterBase from './base'
 import {deepAssign} from '../utils'
@@ -7,9 +8,19 @@ import {deepAssign} from '../utils'
 export class JsonHasPropertiesAsserter extends AsserterBase {
   protected async uniqWork() {
     const sourceJSON = require(path.join(this.templateDir, this.assertion.source_relative_filepath))
-    const targetJSONPath = path.join(this.workingDir, this.assertion.target_relative_filepath)
-    const targetJSON = require(targetJSONPath)
-    const assertedJSON = deepAssign({...targetJSON}, sourceJSON)
-    await fs.writeFile(targetJSONPath, JSON.stringify(assertedJSON, null, 2) + '\n')
+    const jsonPaths: string[] = []
+    if (this.assertion.target_relative_filepath) {
+      jsonPaths.push(path.join(this.workingDir, this.assertion.target_relative_filepath))
+    } else if (this.assertion.target_glob_filepath) {
+      const matches = await glob(path.join(this.workingDir, this.assertion.target_glob_filepath))
+      jsonPaths.push(...matches)
+    } else {
+      throw new Error('Neither target_glob_filepath nor target_relative_filepath are provided')
+    }
+    for (const targetJSONPath of jsonPaths) {
+      const targetJSON = require(targetJSONPath)
+      const assertedJSON = deepAssign({...targetJSON}, sourceJSON)
+      await fs.writeFile(targetJSONPath, JSON.stringify(assertedJSON, null, 2) + '\n')
+    }
   }
 }
