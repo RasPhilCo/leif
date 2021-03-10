3f0e166f78164393414588eaddc2702fcafbca6b
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2021-03-10 09:50:26 -0800

    fix: octokit types

--- src/asserters/github.ts
+++ src/asserters/github.ts
@@ -1,4 +1,5 @@
 import {Octokit} from '@octokit/rest'
+import {components} from '@octokit/openapi-types'
 
 import AsserterBase from './base'
 
@@ -6,14 +7,15 @@ const GitHubClient = new Octokit({
   auth: process.env.GITHUB_OAUTH_TOKEN || process.env.GITHUB_TOKEN,
 })
 
+type ReposGetResponse = components['schemas']['repository']
 export class GithubRepoPropertyValueAsserter extends AsserterBase {
   protected async uniqWork() {
     const [owner, repo] = this.repoFullName.split('/')
-    const data = (await GitHubClient.repos.get({owner, repo})).data
-    const property = this.assertion.property as keyof Octokit.ReposGetResponse
+    const data = (await GitHubClient.repos.get({owner, repo})).data as ReposGetResponse
+    const property = this.assertion.property as keyof ReposGetResponse
     const value = this.assertion.value
 
-    if (data[property] === undefined) throw new Error(`Property ${property} not supported in the github API`)
+    if ((data[property] as any) === undefined) throw new Error(`Property ${property} not supported in the github API`)
 
     if (data[property] !== value) {
       const payload: {[key: string]: any} = {owner, repo}
@@ -21,7 +23,7 @@ export class GithubRepoPropertyValueAsserter extends AsserterBase {
       if (this.dryRun) {
         return `In --dry-run mode: set ${property} to ${value} on ${owner}/${repo}`
       }
-      await GitHubClient.repos.update(payload as Octokit.ReposUpdateParams)
+      await GitHubClient.repos.update(payload as any)
       return `Set ${property} to ${value} on ${owner}/${repo}`
     }
     return `The ${property} is already ${value} on ${owner}/${repo}`
