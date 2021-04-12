911e2c7015069a36f14b6ef0dc2a4802071cfec0
RasPhilCo <capt.pn@gmail.com>
2021-04-12 09:34:49 -0700

    fix: cleanup cmd (#75)

--- src/commands/cleanup.ts
+++ src/commands/cleanup.ts
@@ -1,20 +1,27 @@
 import * as fs from 'fs-extra'
-import {Command} from '@oclif/command'
+import {Command, flags} from '@oclif/command'
 import {homedir} from '../utils'
 
 export default class Cleanup extends Command {
-  static description = 'remove managed repos'
+  static description = 'remove repos from the local leif working directory'
 
   static args = [
-    {name: 'scope', description: 'scope of repos to remove'},
+    {name: 'org', description: 'GitHub org/username of repos to remove'},
   ]
 
   static flags = {
+    all: flags.boolean({char: 'a', description: 'remove all orgs'}),
   }
 
   async run() {
-    const {args} = this.parse(Cleanup)
-    const localDir = `${homedir}/.leif/github/${args.scope}`
+    const {args, flags} = this.parse(Cleanup)
+    let localDir = `${homedir}/.leif/github`
+    if (!flags.all) {
+      if (!args.org) this.error('Either an org or --all must be passed')
+      localDir = `${localDir}/${args.org}`
+    }
+    this.log(`Removing ${localDir}`)
     await fs.ensureDir(localDir)
+    await fs.rmdir(localDir)
   }
 }
