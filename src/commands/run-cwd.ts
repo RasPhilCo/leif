a31dcb1aa333ee159201f17cceb74461d4929040
RasPhilCo <capt.pn@gmail.com>
2021-05-14 09:22:01 -0700

    refactor: synchronously run assertions in a shared service (#92)

--- src/commands/run-cwd.ts
+++ src/commands/run-cwd.ts
@@ -1,49 +1,22 @@
 import {Command, flags} from '@oclif/command'
 
 import {Leif} from '../types'
-import {exec, syncProcessArray, indentLog, masterBranchName, prepareWorkflows} from '../utils'
-import {AsserterLookup} from '../asserters'
+import {syncProcessArray, masterBranchName, prepareWorkflows, indentLog} from '../utils'
+import SequenceService from '../sequences'
 
 const runSequenceOnCWD = async (sequence: Leif.Sequence) => {
-  const sequenceLength = sequence.assertions.length
   const branchName = sequence.branch_name || sequence.id
-  const dryRun = sequence.dryRun
   const workingDir = process.cwd()
   const masterMain = masterBranchName(workingDir)
 
-  indentLog(0, `# Running sequence ${sequence.id}`, '')
-  indentLog(0, `## With ${sequence.assertions.length} assertions: `)
-  indentLog(2, ...sequence.assertions.map((a: Leif.Assertion) => '- ' + a.description || a.type), '')
-  indentLog(2, 'On repos:')
-  indentLog(2, workingDir, '')
-  indentLog(4, workingDir)
-
-  for (let i = 0; i < sequenceLength; i++) {
-    const assertion = sequence.assertions[i]
-
-    indentLog(6, `Assert: ${assertion.description} (type: ${assertion.type})`)
-
-    const Asserter = AsserterLookup[assertion.type]
-    if (!Asserter) throw new Error(`Invalid assertion type ${assertion.type}`)
-
-    try {
-      const asserter = new Asserter({
-        assertion,
-        repoFullName: 'foo',
-        dryRun,
-        branchName,
-        workingDir,
-        templateDir: sequence.templateDir,
-      })
-      await asserter.run()
-    } catch (error) {
-      await exec(`git -C ${workingDir} checkout ${masterMain}`)
-      await exec(`git -C ${workingDir} branch -D ${branchName}`)
-      throw error
-    }
-  }
-
-  indentLog(0, '')
+  await SequenceService.runAssertions(sequence.assertions, {
+    repoFullName: 'current-working-directory',
+    dryRun: true,
+    branchName,
+    workingDir,
+    masterMain,
+    templateDir: sequence.templateDir,
+  })
 }
 
 export default class RunCWD extends Command {
