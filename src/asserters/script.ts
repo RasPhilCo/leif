cff20502ea10391fcb52063f3ea675da7627eb9e
Cristian Dominguez <cdominguez@salesforce.com>
2022-04-20 12:43:55 -0300

    chore: eslint

--- src/asserters/script.ts
+++ src/asserters/script.ts
@@ -4,19 +4,15 @@ import {exec} from '../utils'
 export class RunCommandsAsserter extends AsserterBase {
   protected async uniqWork() {
     if (!this.assertion.commands) {
-      throw new Error(`No commands were provided in the sequence.`)
+      throw new Error('No commands were provided in the sequence.')
     }
 
     const commands: string[] = Array.isArray(this.assertion.commands) ?
       this.assertion.commands :
       [this.assertion.commands]
 
-    for (let cmd of commands) {
-      try {
-        await exec(cmd, {cwd: this.workingDir})
-      } catch(err) {
-        throw err
-      }
+    for (const cmd of commands) {
+      await exec(cmd, {cwd: this.workingDir})
     }
   }
 }
